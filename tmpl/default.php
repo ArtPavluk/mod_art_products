7f539330186cfbbbe44fc8cd332d0d5dbf6b13de
pavluk <info@art-pavluk.com>
2018-07-21 11:52:13 +0300

    change version to 1.2.0

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Art products module
- * @version    1.1.2
+ * @version    1.2.0
  * @author     Artem Pavluk - www.art-pavluk.com
  * @copyright  Copyright (c) 2010 - 2018 Private master Pavluk. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
